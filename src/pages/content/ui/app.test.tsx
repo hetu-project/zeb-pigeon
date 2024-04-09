29f58d74ae1d6876360af822fc48051dcf900abe
haike <haike0513@gmail.com>
2024-04-09 16:49:12 +0800

    test: remove useless test

--- src/pages/content/ui/app.test.tsx
+++ src/pages/content/ui/app.test.tsx
@@ -1,16 +1,14 @@
 import { describe, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import App from '@pages/content/ui/app';
+// import { render, screen } from '@testing-library/react';
+// import App from '@pages/content/ui/app';
 
 describe('appTest', () => {
   test('render text', () => {
     // given
-    const text = '';
-
-    // when
-    render(<App />);
-
+    // const text = '';
+    // // when
+    // render(<App />);
     // then
-    screen.getByText(text);
+    // screen.getByText(text);
   });
 });
